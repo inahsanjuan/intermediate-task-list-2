b19f579be54004fdf8e5069427047591b09a0904
Elsie <elsiedelapena.framgia@gmail.com>
2017-04-21 17:39:33 +0800

    The Task Controller

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
+Route::get('/tasks', 'TaskController@index');
+Route::post('/tasks', 'TaskController@store');
+Route::delete('/task/{task}','TaskController@destroy');
