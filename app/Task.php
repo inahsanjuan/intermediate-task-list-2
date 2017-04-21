ea13714632443bba56bca16ae2ab1b566a05f08f
Elsie <elsiedelapena.framgia@gmail.com>
2017-04-21 15:12:18 +0800

    Eloquent Relationships

--- app/Task.php
+++ app/Task.php
@@ -8,4 +8,11 @@ class Task extends Model
 {
     //
     protected $fillable = ['name'];
+
+    /* Get the user that owns the task.*/
+    public function user ()
+    {
+    	return $this->belongsTo(User::class);
+    }
+    
 }
