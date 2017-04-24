881a01bce1c1177f3af2a3b94304af081b069f88
Elsie <elsiedelapena.framgia@gmail.com>
2017-04-24 13:13:32 +0800

    Adding Tasks

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,11 +8,28 @@ class TaskController extends Controller
 {
     public function __construct()
     {
-    	$this->middleware('auth');
+        $this->middleware('auth');
     }
 
     public function index(Request $request)
     {
-    	return view('tasks.index');
+        $tasks = $request->user()->tasks()->get();
+
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:255',
+        ]);
+
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+    }
+
+    return redirect('/tasks');
 }
