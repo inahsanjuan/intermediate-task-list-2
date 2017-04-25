86358db0e4a377c91e2bcab4e5b88a1510ee5d6f
Elsie <elsiedelapena.framgia@gmail.com>
2017-04-25 13:42:36 +0800

    Enhance Front End

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,73 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-offset-3 col-md-6">
+             <div class="panel-body">
+                <!-- Display Validation Errors -->
+                @include('common.errors')
 
-    <div class="panel-body">
-        <!-- Display Validation Errors -->
-        @include('common.errors')
+                <!-- new task form -->
+                <form action="{{ url('task') }}" method="POST" class="form-horizontal">
+                    {{ csrf_field() }}
 
-        <!-- new task form -->
-        <form action="{{ url('task') }}" method="POST" class="form-horizontal">
-            {{ csrf_field() }}
+                    <!-- Task Name -->
+                    <div class="form-group">
+                        <label for="task-name" class="col-sm-3 control-label">Task</label>
 
-            <!-- Task Name -->
-            <div class="form-group">
-                <label for="task-name" class="col-sm-3 control-label">Task</label>
+                        <div class="col-sm-6">
+                            <input type="text" name="name" id="task-name" class="form-control">
+                        </div>
+                    </div>      
 
-                <div class="col-sm-6">
-                    <input type="text" name="name" id="task-name" class="form-control">
-                </div>
-            </div>      
-
-            <!-- Add Task Button -->
-            <div class="form-group">
-                <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> Add Task
-                    </button>
-                </div>
+                    <!-- Add Task Button -->
+                    <div class="form-group">
+                        <div class="col-sm-offset-3 col-sm-6">
+                            <button type="submit" class="btn btn-default">
+                                <i class="fa fa-plus"></i> Add Task
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
-        </form>
-    </div>
-    <!-- TODO: Current Task -->
-     @if (count($tasks) > 0)
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                Current Tasks
-            </div>
-
-            <div class="panel-body">
-                <table class="table table-striped task-table">
-
-                    <!-- Table Headings -->
-                    <thead>
-                        <th>Task</th>
-                        <th>&nbsp;</th>
-                    </thead>
-
-                    <!-- Table Body -->
-                    <tbody>
-                        @foreach ($tasks as $task)
-                            <tr>
-                                <!-- Task Name -->
-                                <td class="table-text">
-                                    <div>{{ $task->name }}</div>
-                                </td>
-
-                                <td>
-                                    <form action="{{ url('task/'. $task->id) }}" method="POST">
-                                        {{ csrf_field() }}
-                                        {{ method_field('DELETE') }}
-                                        <button type="submit" id="delete-task-{{ $task->id }}" class="btn btn-danger">
-                                            <i class="fa fa-btn fa-trash"></i>Delete
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+        <!-- TODO: Current Task -->
         </div>
-    @endif
+    </div>
+</div>
 @endsection
